5e74b73d95a019cd13cdd14baee834c0cd318406
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-07-11 10:09:47 -0500

    intento 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,6 +2,157 @@
 """
 Escriba el codigo que ejecute la accion solicitada.
 """
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def load_data():
+    direction = "files/input/shipping-data.csv"
+    df = pd.read_csv(direction)
+
+    return df
+
+
+def create_visual_for_shipping_per_warehouse(df):
+    df = df.copy()
+    plt.figure()
+
+    counts = df.Warehouse_block.value_counts()
+    counts.plot.bar(
+        title="Shipping per Warehouse",
+        xlabel="Warehouse block",
+        ylabel="Record Count",
+        color="tab:blue",
+        fontsize=8,
+    )
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+
+    output = "docs/"
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    plt.savefig(os.path.join(output, "shipping_per_warehouse.png"), dpi=300)
+    plt.close()
+
+
+def create_visual_for_mode_of_shipment(df):
+    df = df.copy()
+    plt.figure()
+
+    counts = df.Mode_of_Shipment.value_counts()
+    counts.plot.pie(
+        title="Mode of shipment",
+        wedgeprops=dict(width=0.35),
+        ylabel="",
+        colors=["tab:blue", "tab:orange", "tab:green"],
+    )
+
+    output = "docs/"
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    plt.savefig(os.path.join(output, "mode_of_shipment.png"), dpi=300)
+    plt.close()
+
+
+def create_visual_for_average_customer_rating(df):
+    df = df.copy()
+    plt.figure()
+
+    df = (
+        df[["Mode_of_Shipment", "Customer_rating"]]
+        .groupby("Mode_of_Shipment")
+        .describe()
+    )
+    df.columns = df.columns.droplevel()
+    df = df[["mean", "min", "max"]]
+
+    plt.barh(
+        y=df.index.values,
+        width=df["max"].values - 1,
+        left=df["min"].values,
+        height=0.9,
+        color="lightgray",
+        alpha=0.8,
+    )
+
+    colors = [
+        "tab:green" if value >= 3.0 else "tab:orange" for value in df["mean"].values
+    ]
+
+    plt.barh(
+        y=df.index.values,
+        width=df["mean"].values - 1,
+        left=df["min"].values,
+        color=colors,
+        height=0.5,
+        alpha=1.0,
+    )
+
+    plt.title("Average Customer Rating")
+    plt.gca().spines["left"].set_color("gray")
+    plt.gca().spines["bottom"].set_color("gray")
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+
+    output = "docs/"
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    plt.savefig(os.path.join(output, "average_customer_rating.png"), dpi=300)
+    plt.close()
+
+
+def create_visual_for_weight_distribution(df):
+    df = df.copy()
+    plt.figure()
+
+    df.Weight_in_gms.plot.hist(
+        title="Shipped Weight Distribution",
+        color="tab:orange",
+        edgecolor="white",
+    )
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+
+    output = "docs/"
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    plt.savefig(os.path.join(output, "weight_distribution.png"), dpi=300)
+    plt.close()
+
+
+def create_html_dashboard():
+    output = "docs/"
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    file = open(os.path.join(output, "index.html"), "w")
+
+    template = (
+        """<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Shipping Dashboard Example</h1>
+        <div style="width: 45%; float: left;">
+            <img src="shipping_per_warehouse.png" alt="Fig 1">
+            <img src="mode_of_shipment.png" alt="Fig 2">
+        </div>
+        <div style="width: 45%; float: right;">
+            <img src="average_customer_rating.png" alt="Fig 3">
+            <img src="weight_distribution.png" alt="Fig 4">
+        </div>
+    </body>
+</html>"""
+    )
+
+    file.write(template)
+    file.close()
 
 
 def pregunta_01():
@@ -35,3 +186,12 @@ def pregunta_01():
     * Su código debe crear la carpeta `docs` si no existe.
 
     """
+
+    df = load_data()
+
+    create_visual_for_shipping_per_warehouse(df)
+    create_visual_for_mode_of_shipment(df)
+    create_visual_for_average_customer_rating(df)
+    create_visual_for_weight_distribution(df)
+
+    create_html_dashboard()
